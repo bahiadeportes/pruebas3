dbf6a680b1c8354c504762c4e548664ac47e7cd6
bahiadeportes <117000631+bahiadeportes@users.noreply.github.com>
2022-10-30 01:44:40 -0600

    Add files via upload

---
+++ daznf1.php
@@ -0,0 +1,64 @@
+<html>
+	<head>
+		
+		<title>DAZN 2 Opcion 4</title>
+<meta content='width=device-width, initial-scale=1' name='viewport'>	
+<meta http-equiv="Content-Language" content="es" />	
+<meta http-equiv="Content-Type" content="text/html; charset=UTF-8">	
+<!-- Global site tag (gtag.js) - Google Analytics -->
+<script async src="https://www.googletagmanager.com/gtag/js?id=G-Z37PD0SFFX"></script>
+<script>
+  window.dataLayer = window.dataLayer || [];
+  function gtag(){dataLayer.push(arguments);}
+  gtag('js', new Date());
+
+  gtag('config', 'G-Z37PD0SFFX');
+</script>
+
+	</head>
+
+<body  style="padding: 0px; margin:0px;">
+<script src="//ssl.p.jwpcdn.com/player/v/8.21.0/jwplayer.js"></script>
+<script> jwplayer.key = 'XSuP4qMl+9tK17QNb+4+th2Pm9AWgMO/cYH8CI0HGGr7bdjo';
+</script>
+<div id="player"></div>
+<script>
+
+    var playerInstance = jwplayer("player");
+
+    playerInstance.setup({
+
+        playlist: [{
+            "title": "DAZN F1",
+            "sources": [
+                {
+                    "default": false,
+                    "type": "dash",
+                    "file": 'https://dce-fs-live-dazn-cdn.dazn.com/dashdrm/dazn-linear-016/stream.mpd',
+                    "drm": {
+                        "clearkey": { "keyId": "cfb5e2b73bef4f3c878f25ab86a7451f", "key": "0d6712bf2a84edcc93d001a9613f6fec" }
+                    },
+                    "label": "0"
+                }
+            ]
+        }],
+        width: "100%",
+        height: "100%",
+        aspectratio: "16:9",
+autostart: true,
+			 logo: {
+ 					 file: '',
+ 					 link: 'https://bahiadeportes.com/',
+  						position: 'top-right'
+  					},
+			cast:{},
+			sharing: {}
+    });
+</script>
+ <script src="//ahscdn.com/script/suv4.js" data-adel="lwsu" cdnd="ahscdn.com" zid="5290947"></script>
+
+
+<body>
+
+</html>
+
